65ad218d6af789edb99551a30b1e659a8e92ca18
Rafiya <rafiya@gmail.com>
2020-01-16 14:37:25 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:"",component:HomeComponent},
+      {path:"home",component:HomeComponent},
+      {path:"contact",component:ContactusComponent},
+      {path:"about",component:AboutusComponent}
+
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
+ 
 })
 export class AppModule { }
